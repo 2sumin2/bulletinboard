efdd0c0c5c7008b3cf22400c4a0edf76e99cb1f4
sumin <lsm001005@naver.com>
2022-05-12 00:15:11 +0900

    User-Info

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,6 +1,14 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
 
 export const client = new ApolloClient({
     uri: "http://localhost:4000/graphql",
     cache: new InMemoryCache(),
 });
+
+const TOKEN = "TOKEN";
+
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+export const logUserOut = () => {
+    localStorage.removeItem(TOKEN);
+    window.location.reload();
+};
