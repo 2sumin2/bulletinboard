bf9f8241f754d510b7d097dd88636faca457764d
sumin <lsm001005@naver.com>
2022-05-13 00:42:43 +0900

    Confirm user and go to post

--- src/components/DataBoardElement.tsx
+++ src/components/DataBoardElement.tsx
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import { Link } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import { useEffect, useState } from 'react';
 
 interface IDataBoardElement {
     id: number;
@@ -21,8 +22,26 @@ const SEARCH_USER_QUERY = gql`
     }
   }
 `;
+const ME_QUERY = gql`
+  query me($token: String) {
+    me(token: $token) {
+        id      
+        name
+        email
+        company
+        createAt
+        updateAt    
+        }
+  }
+`;
 
 function DataBoardElement({ id, classification, title, authorId, deadline, content, attachedFile, createAt, updateAt }: IDataBoardElement) {
+    const token = localStorage.getItem("TOKEN");
+    const { data: myId } = useQuery(ME_QUERY, {
+        variables: {
+            token
+        },
+    });
     const newDate = new Date().setTime(createAt);
     const date = moment(newDate).format("YYYY-MM-DD");
     const { data, loading, error } = useQuery(SEARCH_USER_QUERY, {
@@ -30,16 +49,23 @@ function DataBoardElement({ id, classification, title, authorId, deadline, conte
             id: authorId
         },
     });
+    const [url, setUrl] = useState("/board");
+    useEffect(() => {
+        if (myId.me.id == authorId) {
+            setUrl("/datapost");
+        }
+        else {
+            setUrl("/board");
+        }
+    }, []);
     return (
-
         <tr>
-            <td><Link to="/datapost" state={{ title, deadline, content, attachedFile }}>{id}</Link></td>
-            <td><Link to="/datapost" state={{ title, deadline, content, attachedFile }}>{classification}</Link></td>
-            <td><Link to="/datapost" state={{ title, deadline, content, attachedFile }}>{title}</Link></td>
-            <td><Link to="/datapost" state={{ title, deadline, content, attachedFile }}>{data?.searchUser?.name}</Link></td>
-            <td><Link to="/datapost" state={{ title, deadline, content, attachedFile }}>{date}</Link></td>
+            <td><Link to={url} state={{ title, deadline, content, attachedFile }}>{id}</Link></td>
+            <td><Link to={url} state={{ title, deadline, content, attachedFile }}>{classification}</Link></td>
+            <td><Link to={url} state={{ title, deadline, content, attachedFile }}>{title}</Link></td>
+            <td><Link to={url} state={{ title, deadline, content, attachedFile }}>{data?.searchUser?.name}</Link></td>
+            <td><Link to={url} state={{ title, deadline, content, attachedFile }}>{date}</Link></td>
         </tr>
-
     );
 };
 
